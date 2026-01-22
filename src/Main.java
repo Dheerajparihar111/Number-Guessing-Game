4ed4920938350c66366627233fa26101dcc545c5
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-01-22 22:27:24 +0530

    added EXIT option stop game

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ class level{
         System.out.println("1)EASY");
         System.out.println("2)MEDIUM");
         System.out.println("3)HARD");
+        System.out.println("4)EXIT");
     }
 }
 public class Main {
@@ -13,45 +14,61 @@ public class Main {
         Scanner input = new Scanner(System.in);
         level Level = new level();
         System.out.println("*******************Wel-Come****************");
-        Level.level();
-        int levelChoice = input.nextInt();
-        switch (levelChoice) {
-            case 1:
-            System.out.println("You Get Three Chances To Guess the Number ");
-            for (int i = 0; i < 3; i++) {
-                System.out.println("Enter Your Choice");
-                int choice = input.nextInt();
-                if (choice == Math.floor(random() * 100)) {
-                    System.out.println("Correct Answer");
-                } else {
-                    System.out.println("Try Again");
+        while (true) {
+            Level.level();
+            int levelChoice = input.nextInt();
+            switch (levelChoice) {
+                case 1:
+                    System.out.println("You Get Three Chances To Guess the Number ");
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println("Enter Your Choice");
+                        int choice = input.nextInt();
+                        if (choice == Math.floor(random() * 100)) {
+                            System.out.println("Correct Answer");
+                        } else {
+                            System.out.println("Try Again");
+                        }
+                    }
+                    System.out.println("The Number Was =" + Math.floor(random() * 100));
+                    System.out.println("Better Luck Next Time");
+                    break;
+                case 2:
+                    System.out.println("You Get Six Chances To Guess the Number ");
+                    for (int i = 0; i < 6; i++) {
+                        System.out.println("Enter Your Choice");
+                        int choice = input.nextInt();
+                        if (choice == Math.floor(random() * 100)) {
+                            System.out.println("Correct Answer");
+                        } else {
+                            System.out.println("Try Again");
+                        }
+
+                    }
+                    System.out.println("The Number Was =" + Math.floor(random() * 100));
+                    System.out.println("Better Luck Next Time");
+                    break;
+                case 3:
+                    System.out.println("You Get Ten Chances To Guess the Number ");
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Enter Your Choice");
+                        int choice = input.nextInt();
+                        if (choice == Math.floor(random() * 100)) {
+                            System.out.println("Correct Answer");
+                        } else {
+                            System.out.println("Try Again");
+                        }
+
+                    }
+                    System.out.println("The Number Was =" + Math.floor(random() * 100));
+                    System.out.println("Better Luck Next Time");
+                    break;
+                case 4:{
+                    System.out.println("Thank You For Using Number Guessing Game");
+                    System.exit(0);
                 }
-            }break;
-            case 2:
-                System.out.println("You Get Six Chances To Guess the Number ");
-                for (int i = 0; i < 6; i++) {
-                    System.out.println("Enter Your Choice");
-                    int choice = input.nextInt();
-                    if (choice == Math.floor(random() * 100)) {
-                        System.out.println("Correct Answer");
-                    } else {
-                        System.out.println("Try Again");
-                    }
-                }break;
-            case 3:
-                System.out.println("You Get Ten Chances To Guess the Number ");
-                for (int i = 0; i < 10; i++) {
-                    System.out.println("Enter Your Choice");
-                    int choice = input.nextInt();
-                    if (choice == Math.floor(random() * 100)) {
-                        System.out.println("Correct Answer");
-                    } else {
-                        System.out.println("Try Again");
-                    }
-                }break;
+            }
         }
-        System.out.println("The Number Was ="+Math.floor(random()*100));
-        System.out.println("Better Luck Next Time");
+
     }
 }
 
